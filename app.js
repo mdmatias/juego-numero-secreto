3806060da674ef8836f183789da2beb430e9b158
mdmatias <matiasm910@gmail.com>
2024-01-21 21:52:32 -0300

    se agrego condicional a la funcion verificarIntento

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let intentos = 1;
+let numeroSecreto = generarNumeroAleatorio();
+
+function generarNumeroAleatorio() {
+    return Math.floor(Math.random()*10)+1;
+}
 
 /* 
 let titulo = document.querySelector('h1');
@@ -8,37 +14,45 @@ parrafo.innerHTML = "Indica un número del 1 al 10" */
 
 //Esta funcion realiza lo mismo que lo anterior (cargar un titulo y un parrafo) pero a traves de un evento.
 
-function carga(){
-    let titulo = document.querySelector('h1');
-    titulo.innerHTML = "Juego del Número Secreto"
+    /* Esta funcion se podria solicitar al momento de cargar el body por ejemplo (ONLOAD = cargar();)
+    function cargar(){
+        let titulo = document.querySelector('h1');
+        titulo.innerHTML = "Juego del Número Secreto"
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un número del 1 al 10"
-}
+    let parrafo = document.querySelector('p');
+    parrafo.innerHTML = "Indica un número del 1 al 10"
+    }*/
 
-/* Cumple la misma funcion que lo de arriba 
-function carga(elemento, texto){
-    let elementoHTML = document.querySelector('elemento');
-    elementoHTML.innerHTML = "texto"
+// Cumple la misma funcion que lo de arriba 
+function cargar(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
     return;
 }
 
-carga('h1','Juego del Número Secreto');
-carga('p','Indica un número del 1 al 10');
-*/
+cargar('h1',"Juego del Número Secreto");
+cargar('p',"Indica un número del 1 al 10");
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById ('valorUsuario').value);   //get element by ID obtiene los id de las etiquetas que tienen ID en el archivo HTML
-    console.log(numeroSecreto);
+    /*console.log(numeroSecreto);
     console.log(typeof(numeroSecreto)); //muestra por consola de que tipo de dato es numeroSecreto
     console.log(numeroDeUsuario);
     console.log(typeof(numeroDeUsuario));
-    console.log (numeroDeUsuario === numeroSecreto); //El === compara tipo de dato y valor de las variables
+    console.log (numeroDeUsuario === numeroSecreto); //El === compara tipo de dato y valor de las variables*/
+    
+    if (numeroDeUsuario===numeroSecreto) {
+        cargar('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? "intento" : "intentos"}`);
+    }else{
+        if(numeroDeUsuario > numeroSecreto){
+            cargar('p', "El número secreto es menor")
+            
+        }else{
+            cargar('p', "El número secreto es mayor")    
+        }
+        intentos++;
+    }
     return;
 }
 
-let numeroSecreto = generarNumeroAleatorio();
 
-function generarNumeroAleatorio() {
-    return Math.floor(Math.random()*10)+1;
-}
