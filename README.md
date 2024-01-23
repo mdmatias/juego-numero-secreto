87f8358f077dd3ca2f6094190d52c18590c811e8
mdmatias <matiasm910@gmail.com>
2024-01-22 21:15:26 -0300

    edicion de la funcion generarNumeroAleatorio

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # juego-numero-secreto
+Este juego consiste en generar un numero aleatoria y el usuario debe adivinarlo
--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-let intentos = 0;
-let numeroSecreto = 0;
+let intentos = 0; //variable que cuenta la cantidad de intentos que realizo el usuario para adivinar el numero
+let numeroSecreto = 0; //variable que inicia el numero secreto aleatorio en 0 para luego con la funcion generarNumeroAleatorio este adquiera un nuevo valor
+let listaNumerosSorteados = []; //Lista para guardar los numeros aleatorios sorteados
+let numeroMaximo = 10; //variable que indica hasta que valor el usuario debe ingresar un numero
 condicionesIniciales();
 
 function generarNumeroAleatorio() {
-    return Math.floor(Math.random()*10)+1;
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+
+    //si ya sorteamos todos los numeros...
+    if(listaNumerosSorteados.length == numeroMaximo){
+        cargar('p', 'Ya se sortearon todos los números posibles');
+    }else { //sino seguimos jugando    
+    //si el numero generado esta incluido en la lista...
+        if (listaNumerosSorteados.includes(numeroGenerado)){        //metodo include recorre la lista y verifica si existe el numero que pasamos como parametro.
+            return generarNumeroAleatorio (); //recursividad (se llama a si misma la funcion generarNumeroAleatorio)
+        } else{
+            listaNumerosSorteados.push(numeroGenerado); //si no esta en la lista, lo agrega a la lista
+            return numeroGenerado; // devuelve el nuevo numero generado
+        } 
+    }
 }
 
 /* 
@@ -67,7 +84,7 @@ function limpiarInput(){
 //encapsula la actividad de colocar los mensajes titulo y parrafo, generar un numero aleatoria secreto nuevo e iniciar la variable intentos con el valor 1
 function condicionesIniciales(){
     cargar('h1',"Juego del Número Secreto");
-    cargar('p',"Indica un número del 1 al 10");
+    cargar('p',`Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroAleatorio();
     intentos = 1;
 }
